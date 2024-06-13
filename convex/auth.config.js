899af61deee888e5be6341feefcac21bcb88e0e0
Gaurav Arbat <gauravrarbar@gmail.com>
2024-06-13 18:44:23 +0530

    first commit

---
+++ convex/auth.config.js
@@ -0,0 +1,11 @@
+
+
+const authConfig = {
+    providers: [
+        {
+            domain: "https://huge-sailfish-38.clerk.accounts.dev",
+            applicationID: "convex",
+        },
+    ]
+};
+export default authConfig;
---
+++ src/lib/svgs.tsx
@@ -0,0 +1,9 @@
+export const MessageSeenSvg = (props: React.SVGProps<SVGSVGElement>) => (
+	<svg width={16} height={11} fill='none' {...props}>
+		<title>{"msg-dblcheck"}</title>
+		<path
+			fill='currentColor'
+			d=''
+		/>
+	</svg>
+);
