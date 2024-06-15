e9923842f5cf2ab5c0ecdf205499c02e3adb0b47
Gaurav Arbat <161690106+GauravArbat@users.noreply.github.com>
2024-06-16 03:06:24 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,13 +6,13 @@ import RightPanel from "@/components/home/right-panel";
 import { useTheme } from "next-themes";
 
 export default function Home() {
-	const [isLeftPanelOpen, setIsLeftPanelOpen] = useState(false);
+	const [isLeftPanelOpen, setIsLeftPanelOpen] = useState(true); // Set to true to open the left panel by default
 
 	const toggleLeftPanel = () => {
 		setIsLeftPanelOpen(!isLeftPanelOpen);
 	};
 
-	const handleUserClick = (userId) => {
+	const handleUserClick = (userId: string) => {
 		console.log('User clicked:', userId);
 		setIsLeftPanelOpen(false);
 		// Additional logic to handle opening specific chat can be added here
@@ -25,7 +25,7 @@ export default function Home() {
 				<div className="fixed top-0 left-0 w-full h-36 bg-green-primary dark:bg-transparent -z-30" />
 				{!isLeftPanelOpen && (
 					<button
-						className={`absolute top-4 left-4 p-2 rounded md:hidden z-50 bg-gray-200 dark:bg-gray-800 style6`}
+						className="absolute top-4 left-4 p-2 rounded md:hidden z-50 bg-gray-200 dark:bg-gray-800 style6"
 						onClick={toggleLeftPanel}
 					>
 						<div className="w-6 h-0.5 bg-black dark:bg-white mb-1"></div>
@@ -34,14 +34,12 @@ export default function Home() {
 					</button>
 				)}
 				<div
-					className={`fixed top-0 left-0 h-full w-full md:w-1/4 bg-white dark:bg-gray-900 transition-transform transform ${isLeftPanelOpen ? "translate-x-0" : "-translate-x-full"
-						} md:translate-x-0`}
+					className={`fixed top-0 left-0 h-full w-full md:w-1/4 bg-white dark:bg-gray-900 transition-transform transform ${isLeftPanelOpen ? "translate-x-0" : "-translate-x-full"} md:translate-x-0`}
 				>
 					<LeftPanel onUserClick={handleUserClick} />
 				</div>
 				<div
-					className={`fixed top-0 right-0 h-full w-full md:w-3/4 bg-white dark:bg-gray-900 transition-transform transform ${isLeftPanelOpen ? "translate-x-full" : "translate-x-0"
-						} md:translate-x-0`}
+					className={`fixed top-0 right-0 h-full w-full md:w-3/4 bg-white dark:bg-gray-900 transition-transform transform ${isLeftPanelOpen ? "translate-x-full" : "translate-x-0"} md:translate-x-0`}
 				>
 					<RightPanel />
 				</div>
